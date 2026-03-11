47d69ea46265532d003239ea9fa896aa8af7b164
she1by0 <d13410019@ems.npu.edu.tw>
2026-03-11 19:18:42 +0800

    Add files via upload

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string food = Food.Text.Trim();
+
+            if (food == "")
+            {
+                MessageBox.Show("請輸入餐點名稱！");
+            }
+            else if (food == "漢堡")
+            {
+                MessageBox.Show("漢堡賣完了，要不要試試薯條？");
+            }    
+        }
+    }
+}
